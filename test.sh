cc4d7ce9b21e20347c5716c3579b4ea941b3e17c
Bit D13 <bit@d13.co>
2025-09-17 16:49:40 +0200

    fix: brew formula in tap in CD

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+TAP=algorandfoundation/homebrew-node
+
+brew tap-new $TAP
+
+cp ./Formula/algorand.rb "$(brew --repository)/Library/Taps/$TAP/Formula/"
+
+cd "$(brew --repository)/Library/Taps/$TAP"
+
+brew install --build-from-source algorand
+
+brew --prefix algorand --installed
+
+"$(brew --prefix algorand)/bin/algod" -v
+
+brew uninstall algorand
